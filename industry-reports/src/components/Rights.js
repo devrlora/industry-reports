c57f4d7fab0a13c44de3f29dcfb4ecb768b494e4
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-04-29 17:01:45 -0400

    added grid styling

--- industry-reports/src/components/Rights.js
+++ industry-reports/src/components/Rights.js
@@ -3,11 +3,13 @@ import '../styles/Rights.css';
 function Rights() {
   return (
     <div className="container">
-      <div className="images"></div>
-      <div className="images"></div>
-      <div className="images"></div>
-      <div className="images"></div>
-      <div className="text"></div>
+      <div className="images">
+        <div>sdsdf</div>
+        <div>sdfs</div>
+        <div>dwdwd</div>
+      </div>
+      
+      <div className="text">sddas</div>
     </div>
   );
 }
