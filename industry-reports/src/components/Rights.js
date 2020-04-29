4cf79c374ce8eaf74cc3536347c825fee2de92d3
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-04-29 16:28:23 -0400

    made rights js and css

---
+++ industry-reports/src/components/Rights.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Rights() {
+  return (
+    <div>
+      <div className="images"></div>
+      <div className="images"></div>
+      <div className="images"></div>
+      <div className="images"></div>
+      <div className="text"></div>
+    </div>
+  );
+}
+
+export default Rights;
