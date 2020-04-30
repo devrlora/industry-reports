1456db692d365370a2e4ff11ee5efae045929f11
Student_GA113 <Student_GA113@Student-GA113.local>
2020-04-30 17:11:13 -0400

    added background color, sizing and hover effect to text divs

--- industry-reports/src/App.js
+++ industry-reports/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Stakeholders from './Stakeholders';
+import Stakeholders from './components/Stakeholders';
 
 function App() {
   return (
