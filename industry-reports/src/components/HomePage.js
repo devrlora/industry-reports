0f9c139b113fc9669d578d29496be7cb23be8dcd
Student_GA113 <Student_GA113@Student-GA113.local>
2020-04-30 18:00:33 -0400

    resolve merge

---
+++ industry-reports/src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export class HomePage extends Component {
+    render() {
+        return (
+            <div>
+
+                <h4> Home sholud be same as App.js; Right? </h4>
+                
+            </div>
+        )
+    }
+}
