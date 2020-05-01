868caa5c152e0bd02c49d60f58ed5aadc22410ee
Student_M7 <Student_M7@PD-DAL-M7.local>
2020-05-01 14:32:17 -0400

    App_js Refactored to unbreak code, Modifications to Navigation_js Requiring Denisse's Approval, DummyPages created for Connecting Imports Properly

--- industry-reports/src/App.js
+++ industry-reports/src/App.js
@@ -1,61 +1,50 @@
-// import React from 'react';
-// <<<<<<< dj
-// import './App.css';                                             //  From Rafaelle 
-// import {BrowserRouter as Router,Route} from 'react-DOM';
-// import Navigation from 'Navigation';                          //  From Denisse 
-// import HomePage from './components/HomePage';                //  From 'Deji 
-// import Rights from './components/Rights';                   //  From Walter 
-// import Stakes from './components/Stakes';                  //  From Walter 
-// import Conclusion from './components/Conclusion';         //  From 'Deji 
-// import References from './components/References';        //  Stretch Goal 
-
-
-// function App() {
-//   return (
-//     <React.Fragment>
-//       <div className="App">
-        
-//         <Layout>
-//           <Header>
-//               <Navigation/>
-//           </Header>
-
-//           <section>
-//             <Intro className="Intro"/>
-//           </section>
-            
-//           <section>
-//             <Home className="Home"/>
-//           </section>
-
-//           <section>
-//             <Rights className="Rights"/>
-//           </section>
-
-//           <section>
-//             <Stakeholders className="Stakeholders"/>      
-//           </section>
-
-//           <section>
-//             <Conclusion className="Conclusion"/>
-//           </section>
-            
-//         </Layout>
-
-//       </div>
-//     </React.Fragment>
+import React, {Component} from 'react';
+//import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import logo from './logo.svg';
-import './App.css';
-import Stakeholders from './components/Stakeholders';
-import Rights from './components/Rights';
+import './App.css';                                             //  From Rafaelle 
+import Navigation from './components/Navigation';              //  From Denisse 
+import Home from './components/Home';                         //  From 'Deji 
+import Rights from './components/Rights';                    //  From Walter 
+import Stakeholders from './components/Stakeholders';       //  From Walter 
+import Conclusion from './components/Conclusion';          //  From 'Deji 
+import References from './components/References';       //  Stretch Goal 
 
-function App() {
-  return (
-    <div className="App">
-    <Stakeholders />
-       <Rights />
-    </div>
-  );
+
+
+class App extends Component{
+  render() {
+    return (
+      <React.Fragment>
+        <div className="App">
+          
+            <header>
+                <Navigation/>
+            </header>
+
+            <section>
+              <Home className="Home"/>
+            </section>
+
+            <section>
+              <Rights className="Rights"/>
+            </section>
+
+            <section>
+              <Stakeholders className="Stakeholders"/>      
+            </section>
+
+            <section>
+              <Conclusion className="Conclusion"/>
+            </section>
+
+            <section>
+              <References className="References"/>
+            </section>
+
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
