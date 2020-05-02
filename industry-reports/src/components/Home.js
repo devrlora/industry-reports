e3cc6ba229c2a3bcfcd5126c6743e3616cf1888f
Student_M7 <Student_M7@PD-DAL-M7.local>
2020-05-01 23:35:03 -0400

    TnT

--- industry-reports/src/components/Home.js
+++ industry-reports/src/components/Home.js
@@ -4,7 +4,28 @@ export default function Home() {
     return (
         <div>
 
-            <h4> Content Coming Soon </h4>
+            <h4> 
+                
+            <h1>HomePageLoading</h1>
+
+            <p>
+                Today is AI debates are searching for principles to govern emerging and future tech-
+                nological systems for the common good. If the “good” involves upholding human
+                dignity, then the international human rights system is fit for purpose. If AI researchers,
+                developers, and designers work to protect and respect fundamental human rights,
+                they could open the path for broad social benefit. To disregard human rights would be
+                to close off that path.
+            </p>
+
+            <h1>
+                IF AI RESEARCHERS, DEVELOPERS, AND
+                DESIGNERS WORKED TO PROTECT AND RESPECT
+                FUNDAMENTAL HUMAN RIGHTS, IT COULD OPEN
+                THE PATH FOR SOCIAL BENEFIT. TO DISREGARD
+                HUMAN RIGHTS WOULD BE TO CLOSE OFF THAT PATH.
+            </h1>
+
+             </h4>
             
         </div>
     )
